2b1183ab8175cf58cf57563682867b0cd6e4f2c8
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-12 20:28:51 +0000

    Deploy dontpanic92/ctf-writeups to github.com/dontpanic92/ctf-writeups.git:gh-pages

---
+++ README.md
@@ -0,0 +1,13 @@
+# ctf-writeups ![travis-ci](https://api.travis-ci.org/dontpanic92/ctf-writeups.svg?branch=master)
+
+dontpan1c 的 CTF 笔记
+
+### Disclaimer
+
+本站不包含明令禁止公开解题过程的题目。
+
+### Licenses
+
+|网站代码 | 文章内容|
+|----|----|
+|MIT License | CC BY-NC-SA 4.0|
---
+++ assets/js/index.js
@@ -0,0 +1,44 @@
+function filterTags(tag)
+{
+    var allPosts = document.querySelectorAll('[data-tag]');
+    for (var post of allPosts)
+    {
+        var t = post.getAttribute('data-tag');
+        if (tag != null && t != tag)
+        {
+            post.classList.add('d-none');
+        }
+        else
+        {
+            post.classList.remove('d-none');
+        }
+    }
+
+    var allNavTags = document.querySelectorAll('[data-nav-tag]');
+    for (var a of allNavTags)
+    {
+        var t = a.getAttribute('data-nav-tag');
+        if ((tag != null && t == tag) || (tag == null && t == 'all'))
+        {
+            a.classList.add('active');
+        }
+        else
+        {
+            a.classList.remove('active');
+        }
+    }
+}
+
+function initFlags()
+{
+    var elems = document.body.getElementsByTagName("flag");
+    for (var e of elems)
+    {
+        e.style.backgroundColor = "#000";
+        e.onmouseover = function() { e.style.color = "#FFF"; };
+        e.onmouseleave = function() { e.style.color = "#000"; };
+        e.onmouseleave();
+    }
+}
+
+window.onload=initFlags;
